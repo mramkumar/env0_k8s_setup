bf88965498cc122c2b67cf14143cbb2183d64f38
mramkumar <ramkumarm.eng@gmail.com>
2020-06-24 16:15:20 +0530

    created public subnet

--- subnets.tf
+++ subnets.tf
@@ -4,6 +4,16 @@ resource "aws_subnet" "private-subnets" {
   cidr_block = each.value
   availability_zone = each.key
   tags = {
-    Name = var.environment_name-private-each.key
+     Name = format("%s-%s-%s",var.environment_name,"private",each.key)
+  }
+}
+
+resource "aws_subnet" "public-subnets" {
+  for_each = var.public_subnets
+  vpc_id     = aws_vpc.main.id
+  cidr_block = each.value
+  availability_zone = each.key
+  tags = {
+     Name = format("%s-%s-%s",var.environment_name,"public",each.key)
   }
 }
