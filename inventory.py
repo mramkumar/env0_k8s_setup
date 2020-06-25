0e313e8cf7cb38dcf8e63cd79b31225697905cdf
mramkumar <ramkumarm.eng@gmail.com>
2020-06-25 13:48:21 +0530

    fixed script to wait untill ec2 state become running

--- inventory.py
+++ inventory.py
@@ -1,52 +1,77 @@
 #!/usr/bin/env python
-
 import json
 import subprocess
 
-cmd = ["terraform","state","pull"]
-tf_output = json.loads(subprocess.check_output(cmd))
-f= open("hosts","w+")
-bastion = []
-master = []
-worker = []
-for item in tf_output['resources']:
-   if item['type'] == "aws_instance":
-      for ip in item['instances']:
-         if item['name'].startswith('bastion'):
-            bastion.append(ip['attributes']['public_dns'])
-         elif item['name'].startswith('master'):
-            master.append(ip['attributes']['private_dns'])
-         elif item['name'].startswith('worker'):
-            worker.append(ip['attributes']['private_dns'])
+def run_tf_cmd():
+    global tf_output
+    cmd = ["terraform","state","pull"]
+    tf_output = json.loads(subprocess.check_output(cmd))
+
+def check_ec2_state():
+
+    run_tf_cmd()
+    list_i = []
+
+    for item in tf_output['resources']:
+       if item['type'] == "aws_instance":
+          for ip in item['instances']:
+             if ip['attributes']['instance_state'] == "running":
+                 list_i.append(ip['attributes']['private_dns'])
+    while len(list_i) == 0:
+       check_ec2_state()
 
 
-# bastion node
-f.write("[bastion]\n")
-for ip in bastion:
-   f.write(ip + "\n")
+def write_to_file():
+    check_ec2_state()
 
-# master
-f.write("[kube-master]\n")
-for ip in master:
-   f.write(ip + "\n")
+    f= open("hosts","w+")
 
-# etcd
-f.write("[etcd]\n")
-for ip in master:
-   f.write(ip + "\n")
+    bastion = []
+    master  = []
+    worker  = []
 
-# worker
-f.write("[kube-node]\n")
-for ip in worker:
-   f.write(ip + "\n")
+    for item in tf_output['resources']:
+       if item['type'] == "aws_instance":
+          for ip in item['instances']:
+            if item['name'].startswith('bastion'):
+                bastion.append(ip['attributes']['public_dns'])
+            elif item['name'].startswith('master'):
+                master.append(ip['attributes']['private_dns'])
+            elif item['name'].startswith('worker'):
+                worker.append(ip['attributes']['private_dns'])
 
-f.write("[calico-rr]\n")
-f.write("[k8s-cluster:children]\n")
-f.write("kube-master\n")
-f.write("kube-node\n")
-f.write("calico-rr\n")
 
-f.write("[all:vars]\n")
-f.write("ansible_ssh_common_args='-o StrictHostKeyChecking=no'\n")
+    # bastion node
+    f.write("[bastion]\n")
+    for ip in bastion:
+       f.write(ip + "\n")
 
-f.close()
+    # master
+    f.write("[kube-master]\n")
+    for ip in master:
+       f.write(ip + "\n")
+
+    # etcd
+    f.write("[etcd]\n")
+    for ip in master:
+       f.write(ip + "\n")
+
+    # worker
+    f.write("[kube-node]\n")
+    for ip in worker:
+       f.write(ip + "\n")
+
+    f.write("[calico-rr]\n")
+    f.write("[k8s-cluster:children]\n")
+    f.write("kube-master\n")
+    f.write("kube-node\n")
+    f.write("calico-rr\n")
+
+    f.write("[all:vars]\n")
+    f.write("ansible_ssh_common_args='-o StrictHostKeyChecking=no'\n")
+
+    f.close()
+
+
+if __name__ == "__main__":
+   write_to_file()
